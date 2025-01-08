d696f53e807b0e565f81acc43504afdd839cc2ad
aeott158 <andrew.ott@tflgroup.com>
2025-01-07 19:51:14 -0600

    Create RockPaperScissors.py
    First stab at creating rock, paper, scissors in VERY basic Python.

---
+++ RockPaperScissors.py
@@ -0,0 +1,69 @@
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+
+'''
+
+##### inputs
+
+#player decision
+player_move = int(input("What do you chose?\nType '0' for Rock\nType '1' for Paper\nType '2' for Scissors\n"))
+if player_move == 0:
+    print(f"You threw rock.{rock}")
+elif player_move == 1:
+    print(f"You threw paper.{paper}")
+elif player_move == 2:
+    print(f"You threw scissors.{scissors}")
+else:
+    print("Please try again.")
+    exit()
+
+
+# computer prng decision
+import random
+computer_move = (random.randint(0,2))
+if computer_move == 0:
+    print(f"Your opponent threw rock.{rock}")
+elif computer_move == 1:
+    print(f"Your opponent threw paper.{paper}")
+elif computer_move == 2:
+    print(f"Your opponent threw scissors.{scissors}")
+
+
+##### Game outcome logic
+
+# Tie Condition
+if player_move == computer_move:
+    print("Draw.")
+
+# Player Win
+elif (player_move == (computer_move + 1)) or (player_move == (computer_move - 2)):
+    print("You win.")
+
+# Computer Win
+elif (player_move == (computer_move - 1)) or (player_move == (computer_move + 2)):
+    print("You lose.")
